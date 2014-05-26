1bc22b02bdd7ec28d623d920293e366327232887
davva <dd@usenetmap.com>
2014-05-26 13:07:11 +0200

    asdf
    asdf

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
-$(function(){$(window).load(function(){setTimeout(function(){$(".header-share a").removeClass("rollin-transition");$(".header-info").removeClass("rollin-transition-out")},2E3)});$(document).ready(function(){$("#providerTable").dataTable({"bPaginate":false,"bFilter":false,"bInfo":false});$(window).scroll(function(){if($(this).scrollTop()>400)$(".arrow-up").fadeIn();else $(".arrow-up").fadeOut()});$(".arrow-up").click(function(){$("html, body").animate({scrollTop:0},800);return false});$(".arrow-down").click(function(){$("html, body").animate({scrollTop:800},
-800);return false});$(".header-info").click(function(){$("html, body").animate({scrollTop:$(document).height()},800);return false})})}());
-
+$(function () {
+    $(window).load(function () {
+    	$("body").removeClass("preload");
+        setTimeout(function () {
+            $(".header-share a").removeClass("rollin-transition");
+            $(".header-info").removeClass("rollin-transition-out")
+        }, 2E3)
+    });
+    $(document).ready(function () {
+        $("#providerTable").dataTable({
+            "bPaginate": false,
+            "bFilter": false,
+            "bInfo": false
+        });
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 400) $(".arrow-up").fadeIn();
+            else $(".arrow-up").fadeOut()
+        });
+        $(".arrow-up").click(function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 800);
+            return false
+        });
+        $(".arrow-down").click(function () {
+            $("html, body").animate({
+                    scrollTop: 800
+                },
+                800);
+            return false
+        });
+        $(".header-info").click(function () {
+            $("html, body").animate({
+                scrollTop: $(document).height()
+            }, 800);
+            return false
+        })
+    })
+}());
